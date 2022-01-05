4a646ac43c4ab60d23ffb25c7af564db6237722c
00inboxtest <00.inbox.test@gmail.com>
2022-01-05 15:18:38 +0530

    20 push

--- modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sa_id" {
-  value       = local.sa_id
-  description = "Display Name of created Google Service Account"
-}
-
-output "region" {
-  value       = data.google_client_config.google_client.region
-  description = "GCP Region in which GCE VM Instance being created"
-}
--- setup/main.tf
+++ setup/main.tf
@@ -16,4 +16,3 @@ module "kylo_ren" {
   instance_name    = "kylo-ren"
   network_tags     = ["http-server", "https-server"]
 }
-
